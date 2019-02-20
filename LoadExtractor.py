5fdee9e6507ca90acf8e442ac445594497545782
Rahul Sarkar <rs2018@cse.jgec.ac.in>
2019-02-20 21:25:57 +0530

    Create LoadExtractor.py

---
+++ LoadExtractor.py
@@ -0,0 +1,64 @@
+####Importing the necessary libraries####
+import os
+import numpy as np
+from tqdm import tqdm
+import scipy.io.wavfile as wav
+import python_speech_features as speech_features
+
+class LoadExtractor:
+    
+    def __init__(self, path, nfft=610):
+        '''
+        path: path to the folder containing the dataset from TESS
+        nfft: the FFT size, here, default is 610
+        '''
+        self.path=path
+        self.nfft=nfft
+        
+    def loader(self):
+        label = 0
+        sigList = []
+        rateList = []
+        labelList = []
+        classList = os.listdir(self.path)
+        switchLen = len(classList)/2
+        for nextDir in tqdm(os.listdir(self.path)):
+            classFolder = os.path.join(self.path, nextDir)
+            for file in os.listdir(classFolder):
+                if file is not None:
+                    try:
+                        (rate, sig) = wav.read(os.path.join(classFolder, file))
+                        rateList.append(rate)
+                        sigList.append(sig)
+                        labelList.append(label)
+                    except ValueError:
+                        print(os.path.join(classFolder, file)+" is corrupted")
+                        pass
+            label += 1
+            if label == switchLen:
+                label = 0
+        return rateList, sigList, labelList
+    
+    def mfccExtractor(self, sigList, rateList):
+        '''
+        sigList: list of audio signals for which we need to obtain the MFCC features
+        rateList: list of sampling rates of the respective signals
+        '''
+        mfccFeatureList = []
+        for audio in zip(sigList, rateList):
+            audioSig = audio[0]
+            audioRate = audio[1]
+            mfccFeatures = speech_features.mfcc(audioSig, audioRate, nfft = self.nfft)
+            mfccFeatureList.append(mfccFeatures)
+        return mfccFeatureList
+    
+    def deltaFeatureExtractor(self, featureList, interval):
+        '''
+        featureList: the list of feature vectors for which we are to obatin the delta feature vector
+        interval: the number of frames with respect to which the delta value is calculated
+        '''
+        deltafeatureList = []
+        for feature in tqdm(featureList):
+            feature = np.asarray(feature)
+            deltafeatureList.append(speech_features.delta(feature, interval))
+        return deltafeatureList
